86b5d3eebb2a55a6f4759454d832a6545023884c
rileykeane <rileykeane98@gmail.com>
2018-03-08 16:58:52 +1100

    Add Lab 3-4

---
+++ Labs 3 - 4/task_1_part_A.py
@@ -0,0 +1,20 @@
+"""
+@author Riley Keane
+@since 8/3/18
+@modified 8/3/18
+"""
+
+
+def is_a_leap_year(year):
+    if (year % 4 == 0) and (year % 4 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+
+user_year = int(input('Enter a year: '))
+
+if is_a_leap_year(user_year):
+    print('Is a leap year')
+else:
+    print('Is not a leap year')
