a9a75457cb55e1e1ea1c59374fddb5c01716f8c2
Riley Keane <rileykeane98@gmail.com>
2018-03-11 22:17:00 +1100

    Fixed error where if it did't pass first if statement, it terminated

--- Labs 3 - 4/task_1_part_b.py
+++ Labs 3 - 4/task_1_part_b.py
@@ -6,14 +6,22 @@
 
 user_year = int(input("Enter a year: "))
 
-# Checking if the year is divisible by for and not by 100
+# Checking if the year is divisible by 4
 if user_year % 4 == 0:
-    if user_year % 100 != 0:
+    # checking if year is divisible by 100
+    if user_year % 100 == 0:
+        # if the year is divisible by 400, its a leap year
+        if user_year % 400 == 0:
+            print('Is a leap year')
+        else:
+            print('Is not a leap year')
+    # if its not divisible by 100, its a leap year
+    else:
         print('Is a leap year')
-# Checking if the year is divisible by 400
-elif user_year % 400 == 0:
-        print('Is a leap year')
-# if all tests fail, its not a leap year
+# if its not divisible by 4, its not a leap year
 else:
     print('Is not a leap year')
+
+
+
 
