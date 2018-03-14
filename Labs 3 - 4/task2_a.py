e57059b0eba6a8f5d02e66142f92e5757b7af10d
Riley Keane <rileykeane98@gmail.com>
2018-03-14 14:19:35 +1100

    Created better test cases and fixed bug with empty lists.

--- Labs 3 - 4/task2_a.py
+++ Labs 3 - 4/task2_a.py
@@ -2,7 +2,7 @@ import random
 """
 @author Riley Keane
 @since 11/3/18
-@modified 13/3/18
+@modified 14/3/18
 """
 
 
@@ -30,12 +30,16 @@ def my_max(the_list):
     """
     This function, given a list of values, will find and return the max value
     :param the_list: a list of integers
-    :return: current_max: the max value found in the list
+    :return: current_max: the max value found in the list or None if the list is empty
     """
-    current_max = the_list[0]
     list_length = len(the_list)
-    i = 0
 
+    if list_length <= 0:
+        return None
+    else:
+        current_max = the_list[0]
+
+    i = 0
     # loop through the entire list
     while i < list_length:
         # check if the next value if higher then the previous if so, make it the new max
@@ -50,12 +54,15 @@ def my_min(the_list):
     """
     This function, given a list of values, will find and return the min value
     :param the_list: a list of integers
-    :return: current_min: the min value found in the list
+    :return: current_min: the min value found in the list or None if the list is empty
     """
-    current_min = the_list[0]
     list_length = len(the_list)
-    i = 0
+    if list_length <= 0:
+        return None
+    else:
+        current_min = the_list[0]
 
+    i = 0
     # loop through the entire list
     while i < list_length:
         # check if the next value if lower then the previous if so, make it the new min
@@ -69,9 +76,8 @@ def my_min(the_list):
 def test_max():
     """
     This function uses the created my_max function and compares it to pythons max function. It creates a list of
-    random variables and uses both functions. If the returns the same value, then the my_max function passes the test
-    and true is returned
-    :return: true if the test is passed and false if it isn't
+    random variables and uses both functions.
+    :return: An assert if the test fails
     """
     random_vals = []
 
@@ -83,38 +89,38 @@ def test_max():
     max_val = my_max(random_vals)
     py_max = max(random_vals)
 
-    # if both functions have the same value return true else return false
-    if max_val == py_max:
-        return True
-    else:
-        return False
+    assert max_val == py_max, 'Failed on {} - got {}, expected {}'.format(random_vals, max_val, py_max)
+    assert my_max([]) is None, 'Failed on {} - got {}, expected {}'.format([], my_max([]), None)
+    assert my_max([1]) == 1, 'Failed on {} - got {}, expected {}'.format([1], my_max([1]), 1)
 
 
 def test_min():
     """
     This function uses the created my_min function and compares it to pythons min function. It creates a list of
-    random variables and uses both functions. If the returns the same value, then the my_min function passes the test
-    and true is returned
-    :return: true if the test is passed and false if it isn't
+    random variables and uses both functions.
+    :return: An assert if the test fails
     """
-    # generating 50 random values and appending them to a list
     random_vals = []
 
+    # generating 50 random values and appending them to a list
     for i in range(50):
         random_vals.append(random.randrange(0, 1000, 1))
 
-    # using both min functions to find the min
+    # using both max functions to find the max
     min_val = my_min(random_vals)
     py_min = min(random_vals)
 
-    # if both functions have the same value return true else return false
-    if min_val == py_min:
-        return True
-    else:
-        return False
+    assert min_val == py_min, 'Failed on {} - got {}, expected {}'.format(random_vals, min_val, py_min)
+    assert my_min([]) is None, 'Failed on {} - got {}, expected {}'.format([], my_min([]), None)
+    assert my_min([1]) == 1, 'Failed on {} - got {}, expected {}'.format([1], my_min([1]), 1)
 
 
 def main():
+    '''
+    This function uses the function read_list to get a list of temp from the user. It then uses my_max and my_min to
+    find the range between the max value in the list and the min value in the list. It prints the range.
+    :return: NA
+    '''
     # read the list of numbers
     the_list = read_list()
 
@@ -129,6 +135,7 @@ def main():
 
 
 main()
+
 
 
 
