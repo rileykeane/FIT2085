2b23303b2840a62f5ffbb5d75999d3e057470b6f
Riley Keane <rileykeane98@gmail.com>
2018-03-16 19:15:24 +1100

    Completed task2_b

--- Labs 3 - 4/task2_b.asm
+++ Labs 3 - 4/task2_b.asm
@@ -1,18 +1,18 @@
 # @author Riley Keane
 # @since 11/3/18
-# @modified 14/3/18
+# @modified 16/3/18
 
 	.data
-prompt: .asciiz "How many days in the month?: "
-temp:	.asciiz "Enter a recorded temperature: "
+range: 	.asciiz "The range is "
 	
 	.text
-
 main: 	# allocating local vars
 	# the_list
 	# max_temp
+	# min_temp
+	# range
 	addi $fp, $sp, 0
-	addi $sp, $sp, -8
+	addi $sp, $sp, -16
 	
 	# the_list = read_list()
 	# Calling read list function
@@ -20,29 +20,61 @@ main: 	# allocating local vars
 	# saving return value from read_list
 	sw $v0, -4($fp)	
 	
-	# dealocating variables 
-	addi $sp, $sp, 4
-	
 	# max_temp = max(the_list)
+	# allocating space for arguments
+	addi $sp, $sp, -4
+	# pushing argumnets to stack
 	lw $t0, -4($fp) # the _list
 	sw $t0, 0($sp) # arg 1
 	# calling function
 	jal max
-	
 	# clearing arg
 	addi $sp, $sp, 4
-	
 	# storing result 
 	sw $v0, -8($fp)
 	
-	lw $t0, -8($fp)
+	# min_temp = min(the_list)
+	# allocating space for arguments
+	addi $sp, $sp, -4
+	# pushing argumnets to stack
+	lw $t0, -4($fp) # the list
+	sw $t0, 0($sp)	# storing in arg 1
+	#calling function
+	jal min
+	# clearing arg
+	addi $sp, $sp, 4
+	# storing result
+	sw $v0, -12($fp)
+	
+	# range = max_temp - min_temp
+	lw $t0, -8($fp) # max
+	lw $t1, -12($fp) # min
+	# max - min
+	sub $t0, $t0, $t1
+	sw $t0, -16($fp) # range
+	
+	# print('The range is ' + str(range))
+	# printing string
+	addi $v0, $0, 4
+	la $a0, range
+	syscall
+	# pritning range
+	addi $v0, $0, 1
+	lw $a0, -16($fp)
+	syscall
 	
 	# exit
 	addi $v0, $0, 10
 	syscall
 	
-	
+
+# read_list() function	
 read_list: 
+	.data
+	prompt: .asciiz "How many days in the month?: "
+	temp:	.asciiz "Enter a recorded temperature: "
+	
+	.text
 	# saving $ra and $fp	
 	addi $sp, $sp -8
 	sw $ra, 4($sp)
@@ -83,8 +115,9 @@ read_list:
 	# i = 0
 	lw $0, -8($fp)
 	
-	# while i < days
-read_list_loop: 	
+	
+read_list_loop: 
+	# while i < days	
 	lw $t0, -8($fp) # i
 	lw $t1, -16($fp) # days
 	slt $t0, $t0, $t1
@@ -131,6 +164,7 @@ end_read_list:
 	jr $ra
 	
 
+# my_max() function
 max: 	# saving $fp and $ra
 	addi $sp, $sp, -8
 	sw $fp, 0($sp)
@@ -210,20 +244,85 @@ end_max_loop:
 	# jump out of function
 	jr $ra
 	
-	
-	
-	
-	
-	
-	
-	  
-	
-	
 
+# my_min function	
+min: 	# saving $fp and $ra
+	addi $sp, $sp, -8
+	sw $fp, 0($sp)
+	sw $ra, 4($sp)
 	
+	# copy $sp to $fp
+	add $fp, $0, $sp
+	
+	# allocating local var
+	# current_min
+	# list_length
+	# i
+	addi $sp, $sp, -12
+	
+	# list_length = len(the_list)
+	lw $t0, 8($fp)
+	lw $t1, 0($t0)
+	sw $t1, -8($fp)
+	
+	# current_min = the_list[0]
+	lw $t0, 8($fp)
+	lw $t0, 4($t0)
+	sw $t0, -4($fp)
+	
+	# i = 1
+	addi $t0, $0, 1
+	sw $t0, -12($fp)
+	
+loop_min:	
+	# while i < list_length
+	lw $t0, -8($fp) # list_length
+	lw $t1, -12($fp) # i
+	slt $t0, $t1, $t0
+	beq $t0, $0, end_min_loop
+	
+	# if current_min > the_list[i]
+	lw $t0, -4($fp) # current_min
+	lw $t1, 8($fp) # the_list
+	lw $t2, -12($fp) # i
+	sll $t2, $t2, 2
+	add $t1, $t1, $t2
+	lw $t1, 4($t1) # the_list[i]
+	
+	slt $t0, $t1, $t0
+	beq $0, $t0, end_min_if
+	
+	# current_min = list[i]
+	sw $t1, -4($fp) 
+	
+	# i = i + 1
+	lw $t0, -12($fp) # i
+	addi $t0, $t0, 1
+	sw $t0, -12($fp)
+	
+	j loop_min
+	
+end_min_if:
+	# i = i + 1
+	lw $t0, -12($fp) # i
+	addi $t0, $t0, 1
+	sw $t0, -12($fp)
+	
+	j loop_min
 
+end_min_loop:
+	# setting $v0 to return value
+	lw $v0, -4($fp)
+	
+	# claering local vars
+	addi $sp, $sp, 12
+	
+	# restoring $ra and $fp
+	lw $fp, 0($sp)
+	lw $ra, 4($sp)
+	addi $sp, $sp, 8
+	
+	# jump out of function
+	jr $ra
 	
 	
-	
-	
-
