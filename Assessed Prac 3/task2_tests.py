8c9de8f51ac21e8bff4e5338b2039faa065f0414
Riley Keane <rileykeane98@gmail.com>
2018-05-11 17:27:38 +1000

    Testing for task2

---
+++ Assessed Prac 3/task2_tests.py
@@ -0,0 +1,16 @@
+from task2 import import_keys
+
+def test_import_keys():
+    test1 = import_keys('test_keys1.txt', 1, 7)
+    test2 = import_keys('test_keys2.txt', 27183, 11)
+    assert test1._array == [None, ('dog', 1), None, ('cat', 1), ('fish', 1), None, None]
+    assert test2._array == [('b', 1), ('abc', 1), None, None, ('aaa', 1), ('bb', 1), None, None, None, None, ('aa', 1)]
+
+
+def main():
+    print('Testing test_import_keys()')
+    test_import_keys()
+    print('\nPassed all tests')
+
+
+main()
